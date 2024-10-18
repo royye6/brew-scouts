ff9e35117d008c3eb5850f80995925a8f2437955
royye6 <royye6@gmail.com>
2024-10-18 19:26:14 +0200

    update path name

--- backend/routes/authRoutes.mjs
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-import { signIn, signUp } from "../controllers/authController.mjs";
-
-const router = express.Router();
-
-router.post("/sign-up", signUp);
-router.post("/sign-in", signIn);
-
-export default router;
--- backend/routes/businessRoutes.mjs
+++
@@ -1,5 +0,0 @@
-import express, { Router } from "express";
-
-const router = express.Router();
-
-export default router;
